8192810b714d9ca901f0bd4f9b583b6094dc7a00
b3rking <ishimweeschylebersy@gmail.com>
2022-02-19 15:21:32 +0200

    implement roles

---
+++ app/Http/Resources/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class User extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'fullname' => $this->fullname,
+            'username' => $this->username,
+            'role' => $this->role->name,
+            'adress' => $this->adress,
+            'number' => $this->number,
+            'avatar' => $this->avatar
+        ];
+    }
+}
