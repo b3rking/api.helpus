110967f03f3b5b5867f650c58a0c688c0fdf7d8e
b3rking <ishimweeschylebersy@gmail.com>
2022-10-16 20:35:46 +0200

    attempt auth login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,9 +4,19 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Traits\HttpResponses;
 
 class AuthController extends Controller
 {
-    
-    
+    use HttpResponses;
+
+
+    public function login(Request $request) {
+        $user = $request->only(['username', 'password']);
+
+        if (Auth::attempt($user)) {
+            HttpResponses::success($user, 'user loged in successfully', 200);
+        }
+    }
 }
--- app/Http/Traits/HttpResponse.php
+++ app/Http/Traits/HttpResponse.php
@@ -5,7 +5,13 @@ namespace App\Traits;
 
 class HttpResponses {
 
-    public function success($data, $message=null, $status_code=200) {
+    /**
+     * 
+     * @param Array $data a array containing to return in response
+     * @param string $message a string with a simple message about the operation
+     * @param int status_code a http code corresponding to the response returned
+     */
+    public static function success(Array $data, string $message='', int $status_code=200) {
         return response([
             'status' => 'success',
             'message' => $message,
@@ -13,7 +19,13 @@ class HttpResponses {
         ], $status_code);
     }
     
-    public function failure($data, $message=null, $status_code=200) {
+    /**
+     * 
+     * @param Array $data a array containing to return in response
+     * @param string $message a string with a simple message about the operation
+     * @param int status_code a http code corresponding to the response returned
+     */
+    public function failure(Array $data, string $message='', int $status_code) {
         return response([
             'status' => 'failed',
             'message' => $message,
