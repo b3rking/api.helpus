8a7bcb20213f4131cdd4ffbdd3e5e943b10c1404
b3rking <ishimweeschylebersy@gmail.com>
2022-11-24 20:48:29 +0200

    feat: remove fortify

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-        FortifyServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
